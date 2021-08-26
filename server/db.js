8d600e3920ae88c085650daf6df525218fd1e7be
Tony Minseok Kim <tonykim.minseok@gmail.com>
2021-08-26 12:08:36 -0700

    adding dotenv file to the server

---
+++ server/db.js
@@ -0,0 +1,14 @@
+const dotenv = require("dotenv");
+dotenv.config();
+// above code can also be done in this way
+// require("dotenv").config();
+
+// const Pool = require("pg").Pool;
+
+// const pool = new Pool({
+//   user: process.env.USER,
+//   password: process.env.PASSWORD,
+//   host: process.env.HOST,
+//   port: process.env.PORT,
+//   database: process.env.DATABASE,
+// });
--- server/index.js
+++ server/index.js
@@ -1,6 +1,26 @@
+// ***** ***** *****
+// server setups
+// ***** ***** *****
 const express = require("express");
-
 const app = express();
+// 'cors' allows different domain applications to interact each other
+// for example, React front-end and PostgreSQL back-end
+const cors = require("cors");
+
+// ***** ***** *****
+// middleware
+// ***** ***** *****
+app.use(cors());
+// gives an access to the 'request body' from the client side to get the data as JSON
+app.use(express.json());
+
+// ***** ***** *****
+// server execution
+// ***** ***** *****
+
+// const db = require("./db");
+// const test = process.env.USER;
+// console.log(test);
 
 app.listen(5000, () => {
   console.log("server has started on port 5000");
